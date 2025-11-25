6d1f444ed80b799f70d9950e5c8f3d40f7672039
Ettore La Luna <e.laluna@studenti.unisa.it>
2025-11-25 19:37:05 +0100

    aggiunto il metodo doSave in tipologia bigliettoDAo

--- src/main/java/model/dao/TipologiaBigliettoDAO.java
+++ src/main/java/model/dao/TipologiaBigliettoDAO.java
@@ -9,6 +9,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TipologiaBigliettoDAO {
 
@@ -37,6 +39,31 @@ public class TipologiaBigliettoDAO {
         return list;
     }
 
-    public void doSaveTipologiaBiglietto(TipologiaBiglietto t2) {
+    public void doSaveTipologiaBiglietto(TipologiaBiglietto tipoBiglietto) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+        String insertSQL = "INSERT INTO tipologia_biglietto (Nome, Prezzo, ID_Museo) VALUES (?, ?, ?)";
+
+        try {
+            connection = ConnPool.getConnection();
+            ps = connection.prepareStatement(insertSQL);
+            ps.setString(1, tipoBiglietto.getNome());
+            ps.setBigDecimal(2, tipoBiglietto.getPrezzo());
+            ps.setInt(3, tipoBiglietto.getIdMuseo());
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            Logger.getLogger(TipologiaBigliettoDAO.class.getName()).log(Level.SEVERE, "Errore nel salvataggio della tipologia biglietto", e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (ps != null) ps.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                Logger.getLogger(TipologiaBigliettoDAO.class.getName()).log(Level.SEVERE, "Errore chiusura risorse", e);
+            }
+        }
     }
 }
